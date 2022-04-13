7b3ae2ef9ad7e51f439062e9b9a041a8223a2752
loubarb <louis.barbati@gmail.com>
2022-04-12 22:26:42 -0400

    Created first componenet

---
+++ src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <p>This is the store picker!</p>
+    )
+  }
+}
+
+export default StorePicker;
