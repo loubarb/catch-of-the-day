eb02092806e65c714987b229e9c97bd7bc5e2c40
loubarb <louis.barbati@gmail.com>
2022-04-14 22:55:47 -0400

    Added refs, enable store picker button to enter store with given store names input text

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    e.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+  
   render() {
     return (
       <>
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" placeholder="Store Name" />
+        <input type="text" ref={this.myInput} placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
       </>
